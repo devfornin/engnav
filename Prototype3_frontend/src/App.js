443cd4f7488386977fb6372eae20e0451ebe85bb
varun <2300090189csith@gmail.com>
2025-04-22 23:21:15 +0530

    Update App.js

--- Prototype3_frontend/src/App.js
+++ Prototype3_frontend/src/App.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import './App.css';
 import TextField from './TextField';
 import NavBar from './NavBar';
 
-// class App extends Component {
-//   state = {  }
-//   render() { 
-//     return (  );
-//   }
-// }
- 
-// export default App;
-
 class App extends Component {
-
   render() {
     return (
-      
-    <div className="App">
-      <div className="App-Component">
-          <h1><NavBar/></h1>
+      <div className="App">
+        <div className="App-Component">
+          <NavBar /> {/* Moved NavBar outside of <h1> */}
+          <h1>Welcome to the App</h1> {/* Heading should be separate */}
           <TextField />
+        </div>
       </div>
-    </div>
-     );
-    }
+    );
+  }
 }
 
 export default App;
